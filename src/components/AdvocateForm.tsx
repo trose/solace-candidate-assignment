620b1425d213f5d3ef702e3a3cd709a2175b240e
Taylor Rose <taylorjrose@gmail.com>
2025-10-03 09:44:00 -0600

    fix: add 'use client' directive to components using React hooks
    - Add 'use client' to AdvancedFilters component
    - Add 'use client' to AdvocateForm component
    - Add 'use client' to Pagination component
    - Add 'use client' to AdvocateTable component
    - Fix Next.js App Router client component requirements
    - Resolve server component import errors

--- src/components/AdvocateForm.tsx
+++ src/components/AdvocateForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { useAdvocateForm } from '../hooks/useAdvocateForm';
 import { AdvocateFormInput } from '../schemas/advocateSchemas';
